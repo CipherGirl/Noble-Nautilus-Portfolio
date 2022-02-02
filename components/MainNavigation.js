e63881677c366d016850c2b2f05ebe1e30e932a8
DinuBotan <dinubotan@gmail.com>
2022-02-02 12:53:34 +0200

    Add navigation bar

---
+++ components/MainNavigation.js
@@ -0,0 +1,28 @@
+import Link from "next/Link";
+
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">NOBLE NAUTILUS</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/members">Members</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ pages/projects/index.js
@@ -0,0 +1,5 @@
+function MembersPage() {
+  return <div>This is projects page, in case we decide to add something here :D</div>;
+}
+
+export default MembersPage;
