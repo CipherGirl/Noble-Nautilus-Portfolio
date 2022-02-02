e696cc4cdf06f0d42f682b37c5349b08f8836118
DinuBotan <dinubotan@gmail.com>
2022-02-02 13:26:05 +0200

    Change projects page function name

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,5 +1,5 @@
-function MembersPage() {
+function ProjectsPage() {
   return <div>This is projects page, in case we decide to add something here :D</div>;
 }
 
-export default MembersPage;
+export default ProjectsPage;
