c2de60df3646851f3dd68f427b5ec5c9b876d8e5
Hasna Hena Mow <ciphergirlmeow@gmail.com>
2022-02-04 19:52:29 +0600

    Added members info

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,19 +1,41 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./MemberCard.module.css";
-import avatar from "../public/images/avatar.png";
+import classes from "./Footer.module.css";
+import {
+  faGithub,
+  faFacebook,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 const MemberCard = ({ member }) => {
   return (
-    <div className="w-full flex items-center justify-evenly text-xl rounded-xl bg-white p-5 bg-opacity-60 backdrop-filter backdrop-blur-lg">
-      <img
-        src={member.avatar}
-        alt="Avatar"
-        style={{ width: "200px" }}
-      />
+    <div className="w-full min-h-full flex gap-5 items-center justify-evenly text-xl rounded-xl shadow-md overflow-hidden md:max-w-2xl bg-white p-5 bg-opacity-60 backdrop-filter backdrop-blur-lg">
+      <div className="">
+        <img
+          className="object-cover h-48 w-64"
+          src={member.avatar}
+          alt="Avatar"
+        />
+      </div>
       <div className={styles.container}>
         <h4>
           <b>{member.name}</b>
         </h4>
-        <h5>{member.location}</h5>
-        <p>{member.tagline}</p>
+        <p className="text-sm text-gray-500">{member.location}</p>
+        <p className="text-lg mt-2">{member.tagline}</p>
+        <div className="flex text-sm gap-x-2">
+          <Link href="https://www.linkedin.com/school/mlh-fellowship/">
+            <FontAwesomeIcon
+              className="text-blue-700"
+              icon={faLinkedin}
+              size="2x"
+            />
+          </Link>
+          <Link href="https://www.facebook.com/MajorLeagueHacking">
+            <FontAwesomeIcon className="" icon={faGithub} size="2x" />
+          </Link>
+        </div>
       </div>
     </div>
   );
